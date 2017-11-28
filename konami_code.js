5199f95f67b65a5ce1622a3b5473b08a611036bc
Kavindu Amarasinghe <amarasinghekavindu@yahoo.com>
2017-11-28 21:45:38 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,11 +2,16 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   var chose = document.querySelector('body');
-  var empArr = [];
+  var empty = 0;
   chose.addEventListener('keydown', function(event){
     var x = (event.which || event.keyCode);
-    for (var i = 0; i < empArr.length; i++){
-      empArr.push(x[i]);
-    }
+      if(code[index] === x){
+        empty++;
+      } else if (empty.length === code.length){
+          alert("It matches");
+          empty = 0;
+      } else {
+          empty = 0;
+      }
   });
 }
