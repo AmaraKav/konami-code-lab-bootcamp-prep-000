9baa7ec76aa2a8a8842a8acc8453b92097d5f67a
Kavindu Amarasinghe <amarasinghekavindu@yahoo.com>
2017-11-28 21:50:16 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   var empty = 0;
   document.body.addEventListener('keydown', function(event){
-    var x = parseInt(event.which || event.keyCode);
+    var x = parseInt(event.which || event.detail);
       if(code[empty] === x){
         empty++;
         if (empty === code.length){
